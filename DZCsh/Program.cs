7cb7543d2a179ee45ba6a4c0e60aa87bc3b45de7
DimaK <k.dima2077@gmail.com>
2020-06-26 23:08:03 +0300

    Добавьте файлы проекта.

---
+++ DZCsh/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZCsh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter some text: ");
+            char input;
+            int spaceCount = 0;
+            do
+            {
+                input = Console.ReadKey().KeyChar;
+                if (input == ' ')
+                    spaceCount++;
+            }
+            while (input != '.');
+
+            Console.WriteLine("Spaces: " + spaceCount);
+        }
+        }
+    }
